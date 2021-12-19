78104c9e3215d8c04017dba94ab5819f25fe9054
Anton Melser <anton@melser.org>
2021-12-19 23:23:47 +0800

    Add hash to readium-bundle.js call
    So it can update with new builds. Otherwise Android basically never
    updates
    
    Signed-off-by: Anton Melser <anton@transcrob.es>

--- frontend/src/contents/books/injectables.ts
+++ frontend/src/contents/books/injectables.ts
@@ -1,4 +1,6 @@
 import { Injectable } from "@d-i-t-a/reader/dist/types/navigator/IFrameNavigator";
+// eslint-disable-next-line no-var
+declare var __webpack_hash__: any;
 
 const origin = window.location.origin;
 //FIXME: check whether origin is necessary at all
@@ -6,31 +8,28 @@ const origin = window.location.origin;
 const injectables: Injectable[] = [
   {
     type: "style",
-    // url: `${origin}/readium-css/ReadiumCSS-before.css`,
     url: `${origin}/ReadiumCSS-before.css`,
     r2before: true,
   },
   {
     type: "style",
-    // url: `${origin}/readium-css/ReadiumCSS-default.css`,
     url: `${origin}/ReadiumCSS-default.css`,
     r2default: true,
   },
   {
     type: "style",
-    // url: `${origin}/readium-css/ReadiumCSS-after.css`,
     url: `${origin}/ReadiumCSS-after.css`,
     r2after: true,
   },
   {
     // transcrobes
     type: "script",
-    url: `${origin}/readium-bundle.js`,
+    // the hash is required or Android basically never updates...
+    url: `${origin}/readium-bundle.js?` + __webpack_hash__,
     r2before: true,
   },
   {
     type: "style",
-    // url: `${origin}/fonts/opendyslexic/opendyslexic.css`,
     url: `${origin}/opendyslexic.css`,
     fontFamily: "opendyslexic",
   },
