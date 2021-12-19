77cd35e32a86472fd994a79f7f428f40bf869ed1
Anton Melser <anton@melser.org>
2021-12-19 10:19:12 +0800

    Add help button to configuration screen
    Signed-off-by: Anton Melser <anton@transcrob.es>

--- frontend/src/configuration/Configuration.tsx
+++ frontend/src/configuration/Configuration.tsx
@@ -3,14 +3,20 @@ import { useSelector, useDispatch } from "react-redux";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
-import { useTranslate, Title } from "react-admin";
+import { useTranslate, Title, TopToolbar } from "react-admin";
 import { makeStyles } from "@material-ui/core/styles";
 import { changeTheme } from "./actions";
 import { AppState, ThemeName } from "../lib/types";
+import { CardHeader } from "@material-ui/core";
+import HelpButton from "../components/HelpButton";
 
 const useStyles = makeStyles({
   label: { width: "10em", display: "inline-block" },
   button: { margin: "1em" },
+  toolbar: {
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
 });
 
 function Configuration(): ReactElement {
@@ -25,30 +31,36 @@ function Configuration(): ReactElement {
     localStorage.setItem("mode", mode); // a bit hacky, probably better somewhere else
     return dispatch(changeTheme(mode));
   }
+  const helpUrl = "https://transcrob.es/page/software/configure/preferences";
 
   return (
-    <Card>
-      <Title title={translate("pos.configuration")} />
-      <CardContent>
-        <div className={classes.label}>{translate("pos.theme.name")}</div>
-        <Button
-          variant="contained"
-          className={classes.button}
-          color={theme === "light" ? "primary" : "default"}
-          onClick={() => handleUpdate("light")}
-        >
-          {translate("pos.theme.light")}
-        </Button>
-        <Button
-          variant="contained"
-          className={classes.button}
-          color={theme === "dark" ? "primary" : "default"}
-          onClick={() => handleUpdate("dark")}
-        >
-          {translate("pos.theme.dark")}
-        </Button>
-      </CardContent>
-      {/* no translations for now
+    <div>
+      <TopToolbar className={classes.toolbar}>
+        <CardHeader title="User preferences" />
+        <HelpButton url={helpUrl} />
+      </TopToolbar>
+      <Card>
+        <Title title={translate("pos.configuration")} />
+        <CardContent>
+          <div className={classes.label}>{translate("pos.theme.name")}</div>
+          <Button
+            variant="contained"
+            className={classes.button}
+            color={theme === "light" ? "primary" : "default"}
+            onClick={() => handleUpdate("light")}
+          >
+            {translate("pos.theme.light")}
+          </Button>
+          <Button
+            variant="contained"
+            className={classes.button}
+            color={theme === "dark" ? "primary" : "default"}
+            onClick={() => handleUpdate("dark")}
+          >
+            {translate("pos.theme.dark")} (Beta!)
+          </Button>
+        </CardContent>
+        {/* no translations for now
             <CardContent>
                 <div className={classes.label}>{translate('pos.language')}</div>
                 <Button
@@ -68,7 +80,8 @@ function Configuration(): ReactElement {
                     fr
                 </Button>
             </CardContent> */}
-    </Card>
+      </Card>
+    </div>
   );
 }
 
