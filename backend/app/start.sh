c2393467f62921cda22693a9b1482f5e35b459d1
Anton Melser <anton@melser.org>
2021-12-22 18:39:27 +0800

    Refactor build system
    There were two ways of building (one in docker-compose and one for prod)
    and we also relied on a base image that is never updated. All (up to
    python:3.9-slim and node:16) is now done "in-house", and a lot of cruft
    has been removed
    
    Signed-off-by: Anton Melser <anton@transcrob.es>

--- backend/app/start.sh
+++ backend/app/start.sh
@@ -7,12 +7,13 @@ MODULE_NAME=${MODULE_NAME:-$DEFAULT_MODULE_NAME}
 VARIABLE_NAME=${VARIABLE_NAME:-app}
 export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
 
-HOST=${LISTEN_ADDRESS:-0.0.0.0}
-PORT=${LISTEN_PORT:-8000}
-LOG_LEVEL=${LOG_LEVEL:-info}
+DEFAULT_GUNICORN_CONF=/app/gunicorn_conf.py
+
+export GUNICORN_CONF=${GUNICORN_CONF:-$DEFAULT_GUNICORN_CONF}
+export WORKER_CLASS=${WORKER_CLASS:-"uvicorn.workers.UvicornWorker"}
 
 PRE_START_PATH=${PRE_START_PATH:-/app/prestart.sh}
 . "$PRE_START_PATH"
 
-# Start Uvicorn directly, nb of workers will equal WEB_CONCURRENCY, if set, or 1
-exec uvicorn --host $HOST --port $PORT --log-level $LOG_LEVEL "$APP_MODULE"
+# Start Gunicorn
+exec gunicorn -k "$WORKER_CLASS" -c "$GUNICORN_CONF" "$APP_MODULE"
--- scripts/build-base.sh
+++ scripts/build-base.sh
@@ -2,7 +2,8 @@
 set -e
 
 TAG=${TRANSCROBES_BUILD_TAG:-$(git describe --tags)}
-MAIN_IMAGE=${TRANSCROBES_DOCKER_REPO}/transcrobes-web:$TAG
+MAIN_IMAGE=${TRANSCROBES_DOCKER_REPO}/transcrobes-base:$TAG
+LATEST_IMAGE=${TRANSCROBES_DOCKER_REPO}/transcrobes-base:latest
 ENVIRONMENT=${ENVIRONMENT:-prod}
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
@@ -10,6 +11,5 @@ BASE_DIR=$(dirname $SCRIPT_DIR)
 
 echo "Building image ${MAIN_IMAGE} with context $BASE_DIR with docker file $BASE_DIR/Dockerfile"
 
-docker build $BASE_DIR -f $BASE_DIR/Dockerfile.web -t ${MAIN_IMAGE} \
-  --build-arg ENVIRONMENT=${ENVIRONMENT} \
-  --build-arg TRANSCROBES_DOCKER_REPO=${TRANSCROBES_DOCKER_REPO}
+docker build $BASE_DIR -f $BASE_DIR/Dockerfile.base -t ${MAIN_IMAGE} -t ${LATEST_IMAGE} \
+  --build-arg ENVIRONMENT=${ENVIRONMENT}
