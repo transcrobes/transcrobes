5e9cbe1cf72f59c353597f718614cf932489be14
Anton Melser <anton@melser.org>
2021-12-22 22:44:53 +0800

    More build improvements
    Signed-off-by: Anton Melser <anton@transcrob.es>

--- scripts/build-backend.sh
+++ scripts/build-backend.sh
@@ -2,8 +2,9 @@
 set -e
 
 TAG=${TRANSCROBES_BUILD_TAG:-$(git describe --tags)}
-MAIN_IMAGE=${TRANSCROBES_DOCKER_REPO}/transcrobes-web:$TAG
-LATEST_IMAGE=${TRANSCROBES_DOCKER_REPO}/transcrobes-web:latest
+TRANSCROBES_BASE_IMAGE_TAG=${TRANSCROBES_BUILD_TAG:-$(git describe --tags)}
+MAIN_IMAGE=${TRANSCROBES_DOCKER_REPO}/transcrobes-backend:$TAG
+LATEST_IMAGE=${TRANSCROBES_DOCKER_REPO}/transcrobes-backend:latest
 ENVIRONMENT=${ENVIRONMENT:-prod}
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
@@ -11,7 +12,7 @@ BASE_DIR=$(dirname $SCRIPT_DIR)
 
 echo "Building image ${MAIN_IMAGE} with context $BASE_DIR with docker file $BASE_DIR/Dockerfile"
 
-docker build $BASE_DIR -f $BASE_DIR/Dockerfile.web -t ${MAIN_IMAGE} -t ${LATEST_IMAGE} \
+docker build $BASE_DIR -f $BASE_DIR/Dockerfile.backend -t ${MAIN_IMAGE} -t ${LATEST_IMAGE} \
   --build-arg ENVIRONMENT=${ENVIRONMENT} \
-  --build-arg TRANSCROBES_BACKEND_IMAGE_TAG=${TRANSCROBES_BACKEND_IMAGE_TAG} \
+  --build-arg TRANSCROBES_BASE_IMAGE_TAG=${TRANSCROBES_BASE_IMAGE_TAG} \
   --build-arg TRANSCROBES_DOCKER_REPO=${TRANSCROBES_DOCKER_REPO}
