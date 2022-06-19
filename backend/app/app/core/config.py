94b8090d647a8facb05afe78da921d3186ce3543
Anton Melser <anton@melser.org>
2022-06-19 23:00:03 +0800

    Reduce default refresh token expiration to a day
    Signed-off-by: Anton Melser <anton@transcrob.es>

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -62,7 +62,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str = secrets.token_urlsafe(32)
     # 60 minutes * 24 hours * 30 days = 30 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
-    REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 30
+    REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24
     SERVER_NAME: str
     SERVER_HOST: AnyHttpUrl
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
