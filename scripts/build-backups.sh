bc8c27070023920e8cf2dd42ceb3103cc54efeb6
Anton Melser <anton@melser.org>
2022-08-24 17:32:25 +0800

    Add option to use non default repos for building
    Signed-off-by: Anton Melser <anton@transcrob.es>

--- scripts/build-backups.sh
+++ scripts/build-backups.sh
@@ -3,10 +3,12 @@ set -e
 
 TAG=${TRANSCROBES_BUILD_TAG:-$(git describe --tags)}
 MAIN_IMAGE=${TRANSCROBES_DOCKER_REPO}/transcrobes-backups:$TAG
+# SOURCES_OVERRIDE http://ftp.cn.debian.org/debian/ for China
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 BASE_DIR=$(dirname $SCRIPT_DIR)
 
 echo $BASE_DIR
 
-docker build $BASE_DIR -f $BASE_DIR/Dockerfile.backups -t ${MAIN_IMAGE}
+docker build $BASE_DIR -f $BASE_DIR/Dockerfile.backups -t ${MAIN_IMAGE} \
+  --build-arg SOURCES_OVERRIDE=${SOURCES_OVERRIDE}
--- scripts/build-base.sh
+++ scripts/build-base.sh
@@ -5,6 +5,7 @@ TAG=${TRANSCROBES_BUILD_TAG:-$(git describe --tags)}
 MAIN_IMAGE=${TRANSCROBES_DOCKER_REPO}/transcrobes-base:$TAG
 LATEST_IMAGE=${TRANSCROBES_DOCKER_REPO}/transcrobes-base:latest
 ENVIRONMENT=${ENVIRONMENT:-prod}
+# SOURCES_OVERRIDE http://ftp.cn.debian.org/debian/ for China
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 BASE_DIR=$(dirname $SCRIPT_DIR)
@@ -12,4 +13,4 @@ BASE_DIR=$(dirname $SCRIPT_DIR)
 echo "Building image ${MAIN_IMAGE} with context $BASE_DIR with docker file $BASE_DIR/Dockerfile"
 
 docker build $BASE_DIR -f $BASE_DIR/Dockerfile.base -t ${MAIN_IMAGE} -t ${LATEST_IMAGE} \
-  --build-arg ENVIRONMENT=${ENVIRONMENT}
+  --build-arg ENVIRONMENT=${ENVIRONMENT} --build-arg SOURCES_OVERRIDE=${SOURCES_OVERRIDE}
