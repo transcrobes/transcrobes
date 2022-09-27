27aa8223bb4d41915e0bde5e795a5ad4e113588e
Anton Melser <anton@melser.org>
2022-09-27 14:11:04 +0800

    Deps updates
    Without rxdb
    
    Signed-off-by: Anton Melser <anton@transcrob.es>

--- frontend/src/database/Database.ts
+++ frontend/src/database/Database.ts
@@ -1,5 +1,5 @@
 import _ from "lodash";
-import { addRxPlugin, clone, createRxDatabase, isRxDatabaseFirstTimeInstantiated, removeRxDatabase } from "rxdb";
+import { addRxPlugin, clone, createRxDatabase, removeRxDatabase } from "rxdb";
 import { RxDBDevModePlugin } from "rxdb/plugins/dev-mode";
 import { getRxStorageDexie } from "rxdb/plugins/dexie";
 import { RxDBMigrationPlugin } from "rxdb/plugins/migration";
