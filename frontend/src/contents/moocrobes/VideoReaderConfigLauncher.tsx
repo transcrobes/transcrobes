dc97aedd6ff5ad81693fa3b6fa84df9ac8310bf5
Anton Melser <anton@melser.org>
2023-06-06 17:27:08 +0800

    More sx migration and refacto
    Signed-off-by: Anton Melser <anton@transcrob.es>

--- frontend/src/contents/moocrobes/VideoReaderConfigLauncher.tsx
+++ frontend/src/contents/moocrobes/VideoReaderConfigLauncher.tsx
@@ -1,25 +1,9 @@
-import { Box, Drawer } from "@mui/material";
-import { makeStyles } from "tss-react/mui";
 import SettingsIcon from "@mui/icons-material/Settings";
+import { Box, Drawer } from "@mui/material";
 import { ReactElement, useState } from "react";
 import { Button, useTranslate } from "react-admin";
 import useWindowDimensions from "../../hooks/WindowDimensions";
 import VideoReaderConfig, { VideoReaderConfigProps } from "./VideoReaderConfig";
-
-const useStyles = makeStyles()((theme) => ({
-  button: {
-    [theme.breakpoints.down("md")]: {
-      "& svg": {
-        fontSize: 15,
-      },
-    },
-    [theme.breakpoints.up("sm")]: {
-      "& svg": {
-        fontSize: 30,
-      },
-    },
-  },
-}));
 
 export default function VideoReaderConfigLauncher({
   id,
@@ -27,7 +11,6 @@ export default function VideoReaderConfigLauncher({
   onSubDelayChange,
 }: VideoReaderConfigProps): ReactElement {
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
-  const { classes: locClasses } = useStyles();
   const open = Boolean(anchorEl);
   const dimensions = useWindowDimensions();
   const width = dimensions.width < 600 ? dimensions.width * 0.8 : "inherit";
@@ -42,7 +25,7 @@ export default function VideoReaderConfigLauncher({
   return (
     <>
       <Button
-        className={locClasses.button}
+        size="large"
         children={<SettingsIcon />}
         label={translate("screens.moocrobes.config.title")}
         onClick={handleClick}
