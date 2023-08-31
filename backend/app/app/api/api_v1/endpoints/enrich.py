3adefd794f2e97f24fed742fdd845eb24d4d8043
Anton Melser <anton@melser.org>
2023-08-31 21:48:20 +0800

    Use fake media_type that cf supports for brotli
    For the db .part files
    
    Signed-off-by: Anton Melser <anton@transcrob.es>

--- backend/app/app/api/api_v1/endpoints/enrich.py
+++ backend/app/app/api/api_v1/endpoints/enrich.py
@@ -162,8 +162,8 @@ def db_export_part(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Invalid request",
         )
-
-    return FileResponse(abspath, media_type="application/vnd.tcdb.part")
+    # this fake media_type means it will get brotli compressed, which gives about a 4x compression!
+    return FileResponse(abspath, media_type="application/vnd.ms-fontobject")
 
 
 @router.post("/lemma_test", response_model=Any, name="lemma_test")
